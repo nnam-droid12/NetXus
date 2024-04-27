02a9613ceef225d8accfdd292086451ea0f58608
willitech <williamnnamani@yahoo.com>
2024-04-27 21:11:45 +0100

    added the getpolylinepoints function

--- lib/pages/mappage_screen.dart
+++ lib/pages/mappage_screen.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'package:http/http.dart' as http;
 // import 'dart:convert';
-
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:location/location.dart';
+
+import '../const.dart';
 
 class ProductLocation extends StatefulWidget {
   const ProductLocation({Key? key}) : super(key: key);
@@ -27,10 +29,18 @@ class _ProductLocationState extends State<ProductLocation> {
 
   LatLng? _currentP = null;
 
+  Map<PolygonId, Polyline> polylines = {};
+
   @override
   void initState() {
     super.initState();
-    getLocationUpdates();
+    getLocationUpdates().then(
+      (_) => {
+        getPolyLinePoints().then((coordinates) => {
+              print(coordinates),
+            }),
+      },
+    );
   }
 
   @override
@@ -111,5 +121,24 @@ class _ProductLocationState extends State<ProductLocation> {
         });
       }
     });
+  }
+
+  Future<List<LatLng>> getPolyLinePoints() async {
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      GOOGLE_MAPS_API_KEY,
+      PointLatLng(_center.latitude, _center.longitude),
+      PointLatLng(_pMicrosoftPark.latitude, _pMicrosoftPark.longitude),
+      travelMode: TravelMode.walking,
+    );
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    } else {
+      print(result.errorMessage);
+    }
+    return polylineCoordinates;
   }
 }
